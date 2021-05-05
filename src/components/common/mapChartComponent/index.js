39551549c0ed0f3e8103a985da04853a9653d541
七鸾 <vd2o@foxmail.com>
2021-05-05 23:30:21 +0800

    update

--- src/components/common/mapChartComponent/index.js
+++ src/components/common/mapChartComponent/index.js
@@ -1,24 +1,8 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import './index.scss'
 
-import * as echarts from 'echarts'
-import * as map from './map'
-import { option } from './options'
-
 const MapChartComponent = props => {
-  const boxRef = useRef(null)
-  useEffect(() => {
-    const myChart = echarts.init(boxRef.current)
-    echarts.registerMap('china', map.data)
-    myChart.setOption(option)
-    myChart.on('click', function (params) {
-      if (params.data) {
-        props.updateStripChart(params.data.list.list)
-      }
-    })
-  }, [])
-
-  return <div ref={boxRef} className="map-chart-box"></div>
+  return <div className="map-chart-box"></div>
 }
 
 export default MapChartComponent
