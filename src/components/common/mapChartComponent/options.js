949b1207148cf40193a063157aa3afde6c36507e
七鸾 <vd2o@foxmail.com>
2021-05-07 23:06:39 +0800

    update

--- src/components/common/mapChartComponent/options.js
+++ src/components/common/mapChartComponent/options.js
@@ -203,15 +203,6 @@ export const option = {
         show: false,
       },
     },
-    itemStyle: {
-      normal: {
-        areaColor: 'rgb(73, 146, 255)',
-        borderColor: 'rgb(9, 54, 95)',
-      },
-      emphasis: {
-        areaColor: '#4042ff',
-      },
-    },
   },
   series: [
     {
@@ -219,27 +210,12 @@ export const option = {
       type: 'scatter',
       coordinateSystem: 'geo',
       data: convertData(list),
-      symbolSize: 12,
-      label: {
-        normal: {
-          show: false,
-        },
-        emphasis: {
-          show: false,
-        },
-      },
-      itemStyle: {
-        emphasis: {
-          borderColor: '#fff',
-          borderWidth: 1,
-        },
-      },
     },
     {
       type: 'map',
       map: 'china',
       geoIndex: 0,
-      aspectScale: 0.75,
+      aspectScale: 0.8,
       tooltip: {
         show: false,
       },
